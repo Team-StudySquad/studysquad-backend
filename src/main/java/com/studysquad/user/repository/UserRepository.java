fd0622ec4d89c077e48e20012cce68597d709f89
jwooo <parkjwoo0515@gmail.com>
2023-06-28 17:49:19 +0900

    feat: UserRepository 생성

---
+++ src/main/java/com/studysquad/user/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.studysquad.user.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.studysquad.user.domain.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findByRefreshToken(String refreshToken);
+
+	Optional<User> findByEmail(String email);
+}
