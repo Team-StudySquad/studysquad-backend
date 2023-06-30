cad6e2aa34fbf84eebe58e142c9a0c31d72a517d
jwooo <parkjwoo0515@gmail.com>
2023-06-30 23:05:59 +0900

    feat: spring security 설정 파일 추가

---
+++ src/main/java/com/studysquad/global/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.studysquad.global.config;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.*;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.formLogin().disable()
+			.httpBasic().disable()
+			.csrf().disable()
+			.headers().frameOptions().disable()
+			.and()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+			.authorizeRequests()
+			.requestMatchers(toH2Console()).permitAll()
+			.mvcMatchers("/api/login").permitAll()
+			.anyRequest().authenticated();
+
+		return http.build();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
