062fa182fa43b824b59552c78b3ceea7b5523b7a
jwooo <parkjwoo0515@gmail.com>
2023-07-02 19:22:03 +0900

    refector: expirationPeriod 상수로 변경

--- src/main/java/com/studysquad/global/security/RefreshToken.java
+++ src/main/java/com/studysquad/global/security/RefreshToken.java
@@ -7,13 +7,12 @@ import lombok.Getter;
 public class RefreshToken {
 	private String header;
 	private String data;
-	private int expirationPeriod;
+	public static final int EXPIRATION_PERIOD = 1209600;
 
 	@Builder
-	public RefreshToken(String header, String data, int expirationPeriod) {
+	public RefreshToken(String header, String data) {
 		this.header = header;
 		this.data = data;
-		this.expirationPeriod = expirationPeriod;
 	}
 
 }
