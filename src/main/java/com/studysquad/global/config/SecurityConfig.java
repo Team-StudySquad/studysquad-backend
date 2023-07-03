379af54acdbc9994c7f3b1e9e896fff08ce5eb2b
jwooo <parkjwoo0515@gmail.com>
2023-07-03 11:11:13 +0900

    feat: AuthController 회원가입 기능 추가

--- src/main/java/com/studysquad/global/config/SecurityConfig.java
+++ src/main/java/com/studysquad/global/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
 			.and()
 			.authorizeRequests()
 			.requestMatchers(toH2Console()).permitAll()
-			.mvcMatchers("/api/login").permitAll()
+			.mvcMatchers("/api/login", "/api/join").permitAll()
 			.anyRequest().authenticated();
 
 		return http.build();
--- src/main/java/com/studysquad/user/controller/AuthController.java
+++ src/main/java/com/studysquad/user/controller/AuthController.java
@@ -14,6 +14,7 @@ import com.studysquad.global.common.SuccessResponse;
 import com.studysquad.global.security.AccessToken;
 import com.studysquad.global.security.RefreshToken;
 import com.studysquad.global.security.Token;
+import com.studysquad.user.dto.JoinRequestDto;
 import com.studysquad.user.dto.LoginRequestDto;
 import com.studysquad.user.service.AuthService;
 
@@ -36,6 +37,17 @@ public class AuthController {
 		return SuccessResponse.<Void>builder()
 			.status(200)
 			.message("로그인 성공")
+			.build();
+	}
+
+	@PostMapping("/api/join")
+	@ResponseStatus(HttpStatus.OK)
+	public SuccessResponse<Void> signUp(@RequestBody @Valid JoinRequestDto joinDto) {
+		authService.join(joinDto);
+
+		return SuccessResponse.<Void>builder()
+			.status(200)
+			.message("회원 가입 성공")
 			.build();
 	}
 
