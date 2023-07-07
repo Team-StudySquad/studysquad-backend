160fd752fd784e9798dcc102f66c210876b18f57
jwooo <parkjwoo0515@gmail.com>
2023-07-07 12:45:38 +0900

    fix: RefreshTokenArgumentResolver에서 Exception 반환하도록 수정

--- src/main/java/com/studysquad/global/util/RefreshTokenArgumentResolver.java
+++ src/main/java/com/studysquad/global/util/RefreshTokenArgumentResolver.java
@@ -9,9 +9,12 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+import com.studysquad.global.error.exception.NotFoundRefreshToken;
 import com.studysquad.global.security.RefreshToken;
 
 public class RefreshTokenArgumentResolver implements HandlerMethodArgumentResolver {
+
+	private static final String REFRESH_TOKEN_COOKIE_NAME = "Authorization-refresh";
 
 	@Override
 	public boolean supportsParameter(MethodParameter parameter) {
@@ -27,17 +30,17 @@ public class RefreshTokenArgumentResolver implements HandlerMethodArgumentResolv
 
 		if (cookies != null) {
 			for (Cookie cookie : cookies) {
-				if (cookie.getName().equals("Authorization-refresh")) {
+				if (cookie.getName().equals(REFRESH_TOKEN_COOKIE_NAME)) {
 					String refreshTokenValue = cookie.getValue();
 
 					return RefreshToken.builder()
-						.header("Authorization-refresh")
+						.header(REFRESH_TOKEN_COOKIE_NAME)
 						.data(refreshTokenValue)
 						.build();
 				}
 			}
 		}
 
-		return null;
+		throw new NotFoundRefreshToken();
 	}
 }
