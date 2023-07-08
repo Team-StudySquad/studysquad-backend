862d6d27e5e9b8c7f72a917613c302c18583d8f9
jwooo <parkjwoo0515@gmail.com>
2023-07-08 18:07:19 +0900

    test: status 타입 HttpStatus로 변경

--- src/test/java/com/studysquad/controller/AuthControllerTest.java
+++ src/test/java/com/studysquad/controller/AuthControllerTest.java
@@ -16,6 +16,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -134,7 +135,7 @@ public class AuthControllerTest {
 	}
 
 	@Test
-	@DisplayName("로그인 성공 시 응답 바디를 리턴해준다.")
+	@DisplayName("로그인 성공 시 응답 바디를 리턴")
 	void afterSuccessLoginReturnResponseBody() throws Exception {
 		userRepository.save(createUser());
 
@@ -148,7 +149,7 @@ public class AuthControllerTest {
 		mockMvc.perform(post("/api/login")
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(json))
-			.andExpect(jsonPath("$.status").value("200"))
+			.andExpect(jsonPath("$.status").value(HttpStatus.OK.value()))
 			.andExpect(jsonPath("$.message").value("로그인 성공"))
 			.andExpect(jsonPath("$.data").isEmpty())
 			.andDo(print());
@@ -174,8 +175,8 @@ public class AuthControllerTest {
 	}
 
 	@Test
-	@DisplayName("존재하지 않는 이메일로 로그인시 응답 바디를 리턴")
-	void failLoginWithNoneExistUsernameReturnResponseBody() throws Exception {
+	@DisplayName("존재하지 않는 이메일로 로그인시 실패 응답 바디를 리턴")
+	void failLoginWithNoneExistUsernameReturnFailResponseBody() throws Exception {
 		userRepository.save(createUser());
 
 		LoginRequestDto login = LoginRequestDto.builder()
@@ -189,7 +190,7 @@ public class AuthControllerTest {
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(json))
 			.andExpect(status().isUnauthorized())
-			.andExpect(jsonPath("$.status").value("401"))
+			.andExpect(jsonPath("$.status").value(HttpStatus.UNAUTHORIZED.value()))
 			.andExpect(jsonPath("$.message").value("아이디/비밀번호가 일치하지 않습니다"))
 			.andExpect(jsonPath("$.validation").isEmpty())
 			.andDo(print());
@@ -216,8 +217,8 @@ public class AuthControllerTest {
 	}
 
 	@Test
-	@DisplayName("일치하지 않는 비밀번호로 로그인 시도시 응답 바디 리턴")
-	void failLoginWithIncorrectPasswordReturnResponseBody() throws Exception {
+	@DisplayName("일치하지 않는 비밀번호로 로그인 시도시 실패 응답 바디 리턴")
+	void failLoginWithIncorrectPasswordReturnFailResponseBody() throws Exception {
 		userRepository.save(createUser());
 
 		LoginRequestDto login = LoginRequestDto.builder()
@@ -231,15 +232,15 @@ public class AuthControllerTest {
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(json))
 			.andExpect(status().isUnauthorized())
-			.andExpect(jsonPath("$.status").value("401"))
+			.andExpect(jsonPath("$.status").value(HttpStatus.UNAUTHORIZED.value()))
 			.andExpect(jsonPath("$.message").value("아이디/비밀번호가 일치하지 않습니다"))
 			.andExpect(jsonPath("$.validation").isEmpty())
 			.andDo(print());
 	}
 
 	@Test
-	@DisplayName("이메일에 빈 문자열로 로그인 시도시 응답 바디 리턴")
-	void failLoginWithEmptyEmailReturnResponseBody() throws Exception {
+	@DisplayName("이메일에 빈 문자열로 로그인 시도시 실패 응답 바디 리턴")
+	void failLoginWithEmptyEmailReturnFailResponseBody() throws Exception {
 		userRepository.save(createUser());
 
 		LoginRequestDto login = LoginRequestDto.builder()
@@ -253,15 +254,15 @@ public class AuthControllerTest {
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(json))
 			.andExpect(status().isBadRequest())
-			.andExpect(jsonPath("$.status").value("400"))
+			.andExpect(jsonPath("$.status").value(HttpStatus.BAD_REQUEST.value()))
 			.andExpect(jsonPath("$.message").value("잘못된 요청입니다"))
 			.andExpect(jsonPath("$.validation.email").value("이메일을 입력해주세요"))
 			.andDo(print());
 	}
 
 	@Test
-	@DisplayName("비밀번호에 빈 문자열로 로그인 시도시 응답 바디 리턴")
-	void failLoginWithEmptyPasswordReturnResponseBody() throws Exception {
+	@DisplayName("비밀번호에 빈 문자열로 로그인 시도시 실패 응답 바디 리턴")
+	void failLoginWithEmptyPasswordReturnFailResponseBody() throws Exception {
 		userRepository.save(createUser());
 
 		LoginRequestDto login = LoginRequestDto.builder()
@@ -275,7 +276,7 @@ public class AuthControllerTest {
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(json))
 			.andExpect(status().isBadRequest())
-			.andExpect(jsonPath("$.status").value("400"))
+			.andExpect(jsonPath("$.status").value(HttpStatus.BAD_REQUEST.value()))
 			.andExpect(jsonPath("$.message").value("잘못된 요청입니다"))
 			.andExpect(jsonPath("$.validation.password").value("비밀번호를 입력해주세요"))
 			.andDo(print());
@@ -314,15 +315,15 @@ public class AuthControllerTest {
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(json))
 			.andExpect(status().isOk())
-			.andExpect(jsonPath("$.status").value("200"))
+			.andExpect(jsonPath("$.status").value(HttpStatus.OK.value()))
 			.andExpect(jsonPath("$.message").value("회원 가입 성공"))
 			.andExpect(jsonPath("$.data").isEmpty())
 			.andDo(print());
 	}
 
 	@Test
-	@DisplayName("중복된 이메일로 가입 시 응답 바디 리턴")
-	void failJoinDuplicateEmailReturnResponseBody() throws Exception {
+	@DisplayName("중복된 이메일로 가입 시 실패 응답 바디 리턴")
+	void failJoinDuplicateEmailReturnFailResponseBody() throws Exception {
 		userRepository.save(createUser());
 
 		JoinRequestDto join = JoinRequestDto.builder()
@@ -337,15 +338,15 @@ public class AuthControllerTest {
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(json))
 			.andExpect(status().isConflict())
-			.andExpect(jsonPath("$.status").value("409"))
+			.andExpect(jsonPath("$.status").value(HttpStatus.CONFLICT.value()))
 			.andExpect(jsonPath("$.message").value("중복된 이메일 입니다"))
 			.andExpect(jsonPath("$.validation").isEmpty())
 			.andDo(print());
 	}
 
 	@Test
-	@DisplayName("중복된 닉네임으로 가입 시 응답 바디 리턴")
-	void failJoinDuplicateNicknameReturnResponseBody() throws Exception {
+	@DisplayName("중복된 닉네임으로 가입 시 실패 응답 바디 리턴")
+	void failJoinDuplicateNicknameReturnFailResponseBody() throws Exception {
 		userRepository.save(createUser());
 
 		JoinRequestDto join = JoinRequestDto.builder()
@@ -360,15 +361,15 @@ public class AuthControllerTest {
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(json))
 			.andExpect(status().isConflict())
-			.andExpect(jsonPath("$.status").value("409"))
+			.andExpect(jsonPath("$.status").value(HttpStatus.CONFLICT.value()))
 			.andExpect(jsonPath("$.message").value("중복된 닉네임 입니다"))
 			.andExpect(jsonPath("$.validation").isEmpty())
 			.andDo(print());
 	}
 
 	@Test
-	@DisplayName("이메일에 빈 문자열로 가입 시 응답 바디 리턴")
-	void failJoinEmptyEmailReturnResponseBody() throws Exception {
+	@DisplayName("이메일에 빈 문자열로 가입 시 실패 응답 바디 리턴")
+	void failJoinEmptyEmailReturnFailResponseBody() throws Exception {
 		JoinRequestDto join = JoinRequestDto.builder()
 			.email("")
 			.password("1234")
@@ -381,15 +382,15 @@ public class AuthControllerTest {
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(json))
 			.andExpect(status().isBadRequest())
-			.andExpect(jsonPath("$.status").value("400"))
+			.andExpect(jsonPath("$.status").value(HttpStatus.BAD_REQUEST.value()))
 			.andExpect(jsonPath("$.message").value("잘못된 요청입니다"))
 			.andExpect(jsonPath("$.validation.email").value("이메일을 입력해주세요"))
 			.andDo(print());
 	}
 
 	@Test
-	@DisplayName("비밀번호에 빈 문자열로 가입 시 응답 바디 리턴")
-	void failJoinEmptyPasswordReturnResponseBody() throws Exception {
+	@DisplayName("비밀번호에 빈 문자열로 가입 시 실패 응답 바디 리턴")
+	void failJoinEmptyPasswordReturnFailResponseBody() throws Exception {
 		JoinRequestDto join = JoinRequestDto.builder()
 			.email("aaa@aaa.com")
 			.password("")
@@ -402,15 +403,15 @@ public class AuthControllerTest {
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(json))
 			.andExpect(status().isBadRequest())
-			.andExpect(jsonPath("$.status").value("400"))
+			.andExpect(jsonPath("$.status").value(HttpStatus.BAD_REQUEST.value()))
 			.andExpect(jsonPath("$.message").value("잘못된 요청입니다"))
 			.andExpect(jsonPath("$.validation.password").value("비밀번호를 입력해주세요"))
 			.andDo(print());
 	}
 
 	@Test
-	@DisplayName("닉네임에 빈 문자열로 가입 시 응답 바디 리턴")
-	void failJoinEmptyNicknameReturnResponseBody() throws Exception {
+	@DisplayName("닉네임에 빈 문자열로 가입 시 실패 응답 바디 리턴")
+	void failJoinEmptyNicknameReturnFailResponseBody() throws Exception {
 		JoinRequestDto join = JoinRequestDto.builder()
 			.email("aaa@aaa.com")
 			.password("1234")
@@ -423,7 +424,7 @@ public class AuthControllerTest {
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(json))
 			.andExpect(status().isBadRequest())
-			.andExpect(jsonPath("$.status").value("400"))
+			.andExpect(jsonPath("$.status").value(HttpStatus.BAD_REQUEST.value()))
 			.andExpect(jsonPath("$.message").value("잘못된 요청입니다"))
 			.andExpect(jsonPath("$.validation.nickname").value("닉네임을 입력해주세요"))
 			.andDo(print());
@@ -444,7 +445,7 @@ public class AuthControllerTest {
 		mockMvc.perform(post("/api/reissue")
 				.cookie(requestCookie))
 			.andExpect(status().isOk())
-			.andExpect(jsonPath("$.status").value("200"))
+			.andExpect(jsonPath("$.status").value(HttpStatus.OK.value()))
 			.andExpect(jsonPath("$.message").value("토큰 재발급 성공"))
 			.andExpect(header().exists(token.getAccessToken().getHeader()))
 			.andExpect(cookie().exists(refreshToken.getHeader()))
@@ -462,8 +463,8 @@ public class AuthControllerTest {
 	}
 
 	@Test
-	@DisplayName("유효하지 않은 토큰으로 요청시 응답 바디 리턴")
-	void failReissueWrongTokenReturnResponseBody() throws Exception {
+	@DisplayName("유효하지 않은 토큰으로 요청시 실패 응답 바디 리턴")
+	void failReissueWrongTokenReturnFailResponseBody() throws Exception {
 		RefreshToken wrongToken = RefreshToken.builder()
 			.header(jwtProvider.getRefreshHeader())
 			.data("wrongData")
@@ -473,7 +474,7 @@ public class AuthControllerTest {
 		mockMvc.perform(post("/api/reissue")
 				.cookie(requestCookie))
 			.andExpect(status().isUnauthorized())
-			.andExpect(jsonPath("$.status").value("401"))
+			.andExpect(jsonPath("$.status").value(HttpStatus.UNAUTHORIZED.value()))
 			.andExpect(jsonPath("$.message").value("유효하지 않는 토큰입니다"))
 			.andExpect(result -> {
 				MockHttpServletResponse response = result.getResponse();
@@ -489,9 +490,9 @@ public class AuthControllerTest {
 	}
 
 	@Test
-	@DisplayName("존재하지 않는 사용자 정보를 가지고 요청")
+	@DisplayName("존재하지 않는 사용자 정보를 가지고 요청시 실패 응답 바디 리턴")
 	@Transactional
-	void failReissueExistTokenReturnResponseBody() throws Exception {
+	void failReissueExistTokenReturnFailResponseBody() throws Exception {
 		User user = userRepository.save(createUser());
 		Token token = jwtProvider.createToken(user.getEmail());
 		RefreshToken refreshToken = token.getRefreshToken();
@@ -503,7 +504,7 @@ public class AuthControllerTest {
 		mockMvc.perform(post("/api/reissue")
 				.cookie(requestCookie))
 			.andExpect(status().isNotFound())
-			.andExpect(jsonPath("$.status").value("404"))
+			.andExpect(jsonPath("$.status").value(HttpStatus.NOT_FOUND.value()))
 			.andExpect(jsonPath("$.message").value("사용자를 찾을 수 없습니다"))
 			.andExpect(result -> {
 				MockHttpServletResponse response = result.getResponse();
@@ -530,7 +531,7 @@ public class AuthControllerTest {
 		mockMvc.perform(post("/api/logout")
 				.header(token.getAccessToken().getHeader(), "Bearer " + token.getAccessToken().getData()))
 			.andExpect(status().isOk())
-			.andExpect(jsonPath("$.status").value("200"))
+			.andExpect(jsonPath("$.status").value(HttpStatus.OK.value()))
 			.andExpect(jsonPath("$.message").value("로그아웃 성공"))
 			.andExpect(jsonPath("$.data").isEmpty())
 			.andDo(print());
@@ -539,14 +540,14 @@ public class AuthControllerTest {
 	}
 
 	@Test
-	@DisplayName("유효하지 않는 토큰으로 로그아웃 요청 시 응답 바디 리턴")
-	void failLogoutInvalidAccessTokenReturnResponseBody() throws Exception {
+	@DisplayName("유효하지 않는 토큰으로 로그아웃 요청 시 실패 응답 바디 리턴")
+	void failLogoutInvalidAccessTokenReturnFailResponseBody() throws Exception {
 		Token token = jwtProvider.createToken("invalidUserData");
 
 		mockMvc.perform(post("/api/logout")
 				.header(token.getAccessToken().getHeader(), "Bearer " + token.getAccessToken().getData()))
 			.andExpect(status().isUnauthorized())
-			.andExpect(jsonPath("$.status").value("401"))
+			.andExpect(jsonPath("$.status").value(HttpStatus.UNAUTHORIZED.value()))
 			.andExpect(jsonPath("$.message").value("인증되지 않은 사용자 입니다."))
 			.andExpect(jsonPath("$.validation").isEmpty())
 			.andDo(print());
