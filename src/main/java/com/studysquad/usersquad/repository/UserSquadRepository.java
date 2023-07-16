d4cf6990b9b61c688a813796724a8987ef5e9b5c
jwooo <parkjwoo0515@gmail.com>
2023-07-16 18:28:38 +0900

    fix: UserSquadRepository findBySquad 메서드 추가

--- src/main/java/com/studysquad/usersquad/repository/UserSquadRepository.java
+++ src/main/java/com/studysquad/usersquad/repository/UserSquadRepository.java
@@ -1,8 +1,11 @@
 package com.studysquad.usersquad.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.studysquad.usersquad.domain.UserSquad;
 
 public interface UserSquadRepository extends JpaRepository<UserSquad, Long>, UserSquadRepositoryCustom {
+	List<UserSquad> findBySquadId(Long squadId);
 }
