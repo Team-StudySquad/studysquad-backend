8fbff9801f79295ce3df4731ae64c7d8dc33b5d1
jwooo <parkjwoo0515@gmail.com>
2023-07-17 15:50:47 +0900

    fix: DTO 클래스 기본 생성자 추가

--- src/main/java/com/studysquad/squad/dto/SquadCreateDto.java
+++ src/main/java/com/studysquad/squad/dto/SquadCreateDto.java
@@ -3,10 +3,13 @@ package com.studysquad.squad.dto;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SquadCreateDto {
 
 	@NotBlank(message = "카테고리를 설정해주세요")
@@ -16,13 +19,13 @@ public class SquadCreateDto {
 	@NotBlank(message = "스쿼드 설명을 작성해주세요")
 	private String squadExplain;
 	@NotNull(message = "역할을 입력해주세요")
-	private boolean isMentor;
+	private boolean mentor;
 
 	@Builder
-	public SquadCreateDto(String categoryName, String squadName, String squadExplain, boolean isMentor) {
+	public SquadCreateDto(String categoryName, String squadName, String squadExplain, boolean mentor) {
 		this.categoryName = categoryName;
 		this.squadName = squadName;
 		this.squadExplain = squadExplain;
-		this.isMentor = isMentor;
+		this.mentor = mentor;
 	}
 }
--- src/main/java/com/studysquad/squad/dto/SquadJoinDto.java
+++ src/main/java/com/studysquad/squad/dto/SquadJoinDto.java
@@ -1,18 +1,21 @@
 package com.studysquad.squad.dto;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SquadJoinDto {
 
-	@NotBlank(message = "역할을 선택해주세요")
-	boolean isMentor;
+	@NotNull(message = "역할을 선택해주세요")
+	private boolean mentor;
 
 	@Builder
-	public SquadJoinDto(boolean isMentor) {
-		this.isMentor = isMentor;
+	public SquadJoinDto(boolean mentor) {
+		this.mentor = mentor;
 	}
 }
