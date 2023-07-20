2ad95f5d4e75da4f029898c7c68f31bb35b330cb
jwooo <parkjwoo0515@gmail.com>
2023-07-20 20:17:03 +0900

    feat: SquadController 진행중인 스쿼드 조회 기능 추가

--- src/main/java/com/studysquad/squad/controller/SquadController.java
+++ src/main/java/com/studysquad/squad/controller/SquadController.java
@@ -3,6 +3,7 @@ package com.studysquad.squad.controller;
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.studysquad.global.common.SuccessResponse;
 import com.studysquad.global.security.Login;
+import com.studysquad.squad.dto.ProcessSquadDto;
 import com.studysquad.squad.dto.SquadCreateDto;
 import com.studysquad.squad.dto.SquadJoinDto;
 import com.studysquad.squad.service.SquadService;
@@ -23,6 +25,18 @@ import lombok.RequiredArgsConstructor;
 public class SquadController {
 
 	private final SquadService squadService;
+
+	@GetMapping("/api/squad/process")
+	@ResponseStatus(HttpStatus.OK)
+	public SuccessResponse<ProcessSquadDto> getProcessSquad(@Login LoginUser loginUser) {
+		ProcessSquadDto responseData = squadService.getProcessSquad(loginUser);
+
+		return SuccessResponse.<ProcessSquadDto>builder()
+			.status(HttpStatus.OK.value())
+			.message("진행중인 스쿼드 조회 성공")
+			.data(responseData)
+			.build();
+	}
 
 	@PostMapping("/api/squad")
 	@ResponseStatus(HttpStatus.CREATED)
