a20744ca52827489509ede93eacb3226fa847c71
jwooo <parkjwoo0515@gmail.com>
2023-08-08 16:29:26 +0900

    feat: MissionController 미션 수정 기능 추가

--- src/main/java/com/studysquad/mission/controller/MissionController.java
+++ src/main/java/com/studysquad/mission/controller/MissionController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.studysquad.global.common.SuccessResponse;
 import com.studysquad.global.security.Login;
 import com.studysquad.mission.dto.MissionCreateDto;
+import com.studysquad.mission.dto.MissionEditDto;
 import com.studysquad.mission.service.MissionService;
 import com.studysquad.user.dto.LoginUser;
 
@@ -36,6 +38,21 @@ public class MissionController {
 		return SuccessResponse.<Void>builder()
 			.status(HttpStatus.CREATED.value())
 			.message("미션 생성 성공")
+			.build();
+	}
+
+	@PatchMapping("/api/squad/{squadId}/mission/{missionId}")
+	@ResponseStatus(HttpStatus.OK)
+	public SuccessResponse<Void> editMission(@PathVariable Long squadId,
+		@PathVariable Long missionId,
+		@RequestBody @Valid MissionEditDto editRequest,
+		@Login LoginUser loginUser) {
+
+		missionService.editMission(squadId, missionId, editRequest, loginUser);
+
+		return SuccessResponse.<Void>builder()
+			.status(HttpStatus.OK.value())
+			.message("미션 수정 성공")
 			.build();
 	}
 }
