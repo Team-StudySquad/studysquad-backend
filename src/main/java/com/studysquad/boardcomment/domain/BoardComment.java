43dfceb1dc37c24b13ea58059cb314b1ec41e363
jwooo <parkjwoo0515@gmail.com>
2023-08-20 16:15:01 +0900

    fix: BoardComment 컬럼 ID 수정

--- src/main/java/com/studysquad/boardcomment/domain/BoardComment.java
+++ src/main/java/com/studysquad/boardcomment/domain/BoardComment.java
@@ -1,46 +1,55 @@
 package com.studysquad.boardcomment.domain;
 
+import static javax.persistence.FetchType.*;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+
 import com.studysquad.board.domain.Board;
 import com.studysquad.user.domain.User;
+
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
-import java.time.LocalDateTime;
-
-import static javax.persistence.FetchType.LAZY;
-
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class BoardComment {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "borad_comment_id")
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "board_comment_id")
+	private Long id;
 
-    @ManyToOne(fetch = LAZY)
-    @JoinColumn(name = "board_id")
-    private Board board;
+	@ManyToOne(fetch = LAZY)
+	@JoinColumn(name = "board_id")
+	private Board board;
 
-    @ManyToOne(fetch = LAZY)
-    @JoinColumn(name = "user_id")
-    private User user;
+	@ManyToOne(fetch = LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
 
-    @Lob
-    @Column(name = "board_comment_content")
-    private String boardCommentContent;
+	@Lob
+	@Column(name = "board_comment_content")
+	private String boardCommentContent;
 
-    private LocalDateTime createAt;
+	private LocalDateTime createAt;
 
-    @Builder
-    public BoardComment(Board board, User user, String boardCommentContent) {
-        this.board = board;
-        this.user = user;
-        this.boardCommentContent = boardCommentContent;
-        this.createAt = LocalDateTime.now();
-    }
+	@Builder
+	public BoardComment(Board board, User user, String boardCommentContent) {
+		this.board = board;
+		this.user = user;
+		this.boardCommentContent = boardCommentContent;
+		this.createAt = LocalDateTime.now();
+	}
 
 }
