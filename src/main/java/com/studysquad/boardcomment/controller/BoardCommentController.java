8aa418434b653bfb09148bc1d3140f34639d2384
jwooo <parkjwoo0515@gmail.com>
2023-10-31 20:54:09 +0900

    feat: BoardCommentController 게시글 댓글 생성 기능 추가

--- src/main/java/com/studysquad/boardcomment/controller/BoardCommentController.java
+++ src/main/java/com/studysquad/boardcomment/controller/BoardCommentController.java
@@ -2,15 +2,22 @@ package com.studysquad.boardcomment.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.studysquad.boardcomment.dto.BoardCommentCreateDto;
 import com.studysquad.boardcomment.dto.BoardCommentResponse;
 import com.studysquad.boardcomment.service.BoardCommentService;
 import com.studysquad.global.common.SuccessResponse;
+import com.studysquad.global.security.Login;
+import com.studysquad.user.dto.LoginUser;
 
 import lombok.RequiredArgsConstructor;
 
@@ -28,6 +35,19 @@ public class BoardCommentController {
 			.status(HttpStatus.OK.value())
 			.message("게시글 댓글 조회 성공")
 			.data(boardCommentService.getBoardComments(boardId))
+			.build();
+	}
+
+	@PostMapping("/api/board/{boardId}/boardcomment")
+	@ResponseStatus(HttpStatus.CREATED)
+	public SuccessResponse<Void> createBoardComment(@PathVariable Long boardId, @Login LoginUser loginUser,
+		@RequestBody @Valid BoardCommentCreateDto createRequest) {
+
+		boardCommentService.createBoardComment(boardId, loginUser, createRequest);
+
+		return SuccessResponse.<Void>builder()
+			.status(HttpStatus.CREATED.value())
+			.message("게시글 댓글 생성 성공")
 			.build();
 	}
 }
