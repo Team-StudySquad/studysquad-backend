e9775c73d4639fcec9c70067a531b7988b6ffb34
jwooo <parkjwoo0515@gmail.com>
2023-10-31 20:50:49 +0900

    test: BoardCommentServiceTest 게시글 댓글 관련 테스트 추가

--- src/test/java/com/studysquad/service/BoardCommentServiceTest.java
+++ src/test/java/com/studysquad/service/BoardCommentServiceTest.java
@@ -5,6 +5,7 @@ import static org.mockito.Mockito.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -15,13 +16,24 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.studysquad.board.domain.Board;
+import com.studysquad.board.repository.BoardRepository;
+import com.studysquad.boardcomment.domain.BoardComment;
+import com.studysquad.boardcomment.dto.BoardCommentCreateDto;
 import com.studysquad.boardcomment.dto.BoardCommentResponse;
 import com.studysquad.boardcomment.repository.BoardCommentRepository;
 import com.studysquad.boardcomment.service.BoardCommentService;
+import com.studysquad.user.domain.User;
+import com.studysquad.user.dto.LoginUser;
+import com.studysquad.user.repository.UserRepository;
 
 @ExtendWith(MockitoExtension.class)
 public class BoardCommentServiceTest {
 
+	@Mock
+	UserRepository userRepository;
+	@Mock
+	BoardRepository boardRepository;
 	@Mock
 	BoardCommentRepository boardCommentRepository;
 	@InjectMocks
@@ -48,5 +60,32 @@ public class BoardCommentServiceTest {
 		assertThat(result.size()).isEqualTo(boardCommentsResponse.size());
 		assertThat(result.get(0)).isEqualTo(boardCommentsResponse.get(0));
 		assertThat(result.get(0).getCreator()).isEqualTo(boardCommentsResponse.get(0).getCreator());
+	}
+
+	@Test
+	@DisplayName("게시글 댓글 생성 성공")
+	void successCreateBoardComment() {
+		User user = User.builder()
+			.email("userA@aaa.com")
+			.nickname("userA")
+			.build();
+		LoginUser loginUser = LoginUser.builder()
+			.email(user.getEmail())
+			.build();
+		Board board = Board.builder()
+			.user(user)
+			.build();
+		BoardCommentCreateDto request = BoardCommentCreateDto.builder()
+			.boardCommentContent("boardCommentContent")
+			.build();
+
+		when(userRepository.findByEmail(loginUser.getEmail()))
+			.thenReturn(Optional.of(user));
+		when(boardRepository.findById(board.getId()))
+			.thenReturn(Optional.of(board));
+
+		boardCommentService.createBoardComment(board.getId(), loginUser, request);
+
+		verify(boardCommentRepository, times(1)).save(any(BoardComment.class));
 	}
 }
