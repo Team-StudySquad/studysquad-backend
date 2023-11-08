4f019dd85dc9f5b54cf1cf83eeb471373bc8c3c6
jwooo <parkjwoo0515@gmail.com>
2023-11-08 22:04:40 +0900

    fix: ApiUrls 경로 추가

--- src/main/java/com/studysquad/global/config/ApiUrls.java
+++ src/main/java/com/studysquad/global/config/ApiUrls.java
@@ -2,5 +2,5 @@ package com.studysquad.global.config;
 
 public class ApiUrls {
 	public static final String[] PERMIT_API_URLS = {"/api/login", "/api/join", "/api/reissue", "/api/squad/recruit",
-		"/api/squad/{squadId}"};
+		"/api/squad/{squadId}", "/api/boards", "/api/board/{boardId}", "/api/board/{boardId}/boardcomments"};
 }
